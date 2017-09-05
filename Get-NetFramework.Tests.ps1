340d803ce3b1024d9eed9e52201ee876fc979f10
jeff buenting <jeffbuenting@gmail.com>
2017-09-05 16:18:37 -0400

    new get-netframework and get-netframwork test

---
+++ Get-NetFramework.Tests.ps1
@@ -0,0 +1,65 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-NetFramework" {
+   
+    # ----- Get Function Help
+    # ----- Pester to test Comment based help
+    # ----- http://www.lazywinadmin.com/2016/05/using-pester-to-test-your-comment-based.html
+
+    Context "Help" {
+        $H = Help Get-NetFramework -Full
+        
+        # ----- Help Tests
+        It "has Synopsis Help Section" {
+            $H.Synopsis | Should Not BeNullorEmpty
+        }
+
+        It "has Description Help Section" {
+            $H.Description | Should Not BeNullorEmpty
+        }
+
+        It "has Parameters Help Section" {
+            $H.Parameters | Should Not BeNullorEmpty
+        }
+
+        # Examples
+        it "Example - Count should be greater than 0"{
+            $H.examples.example.code.count | Should BeGreaterthan 0
+        }
+
+        # Examples - Remarks (small description that comes with the example)
+        foreach ($Example in $H.examples.example)
+        {
+            it "Example - Remarks on $($Example.Title)"{
+                $Example.remarks | Should not BeNullOrEmpty
+            }
+        }
+
+        It "has Notes Help Section" {
+            $H.alertSet | Should Not BeNullorEmpty
+        }
+    } 
+
+    Context Execution {
+        
+        # ----- Finds 2.0
+
+        # ----- Finds 3.0
+
+        
+    }
+
+    Context Output {
+        
+        # ----- Finds 4.5 and above
+        It "Should enter 4.0 if statement" {
+            Mock -CommandName Test-Path -MockWith { 
+                Return $True
+            }
+
+            Mock -CommandName Get-Item
+        }
+    }
+}
---
+++ Get-NetFramework.ps1
@@ -0,0 +1,57 @@
+﻿Function Get-NetFramework {
+
+    <#
+        .Synopsis
+            Gets the version of .Net Framework Installed.
+
+        .Description
+            Returns the version or versions of .NET on a computer.
+
+        .Parameter ComputerName
+            Name of the computer to check.
+
+        .Link
+            .Net 4.0
+
+            https://stackoverflow.com/questions/3487265/powershell-script-to-return-versions-of-net-framework-on-a-machine
+    #>
+
+    [CmdletBindig()]
+    Param (
+        [Parameter (ValueFromPipeline = $true)]
+        [String[]]$ComputerName = $env:COMPUTERNAME
+    )
+        
+    Process {
+        Foreach ( $C in $ComputerName ) {
+            Write-Verbose "Processing $C"
+
+            # ----- version 2.0
+
+            # ----- Version 3.0
+
+            # ----- Version 4.0 and above
+            if ( Test-Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' ) {
+
+                $Net = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse  | Get-ItemProperty -name Version,Release -EA 0 |  Where { $_.PSChildName -match '^(?!S)\p{L}' -and $_.Release -ne $Null } | Select PSChildName, Version, Release, @{
+                    name="Product"
+                    expression={
+                        switch -regex ($_.Release) {
+                            "378389" { [Version]"4.5" }
+                            "378675|378758" { [Version]"4.5.1" }
+                            "379893" { [Version]"4.5.2" }
+                            "393295|393297" { [Version]"4.6" }
+                            "394254|394271" { [Version]"4.6.1" }
+                            "394802|394806" { [Version]"4.6.2" }
+                            "460798" { [Version]"4.7" }
+                            {$_ -gt 460798} { [Version]"Undocumented 4.7 or higher, please update script" }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        Write-Output $Net.Product
+    }
+}
