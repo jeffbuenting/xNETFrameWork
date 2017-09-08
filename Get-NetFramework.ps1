dfddfe1103381cf29f82557160050c7642f9f48c
jeffbuenting <jeffbuenting@gmail.com>
2017-09-08 09:43:49 -0400

    finalized get-netframework

--- Get-NetFramework.ps1
+++ Get-NetFramework.ps1
@@ -65,14 +65,14 @@
                             name="Product"
                             expression={
                                 switch -regex ($_.Release) {
-                                    "378389" { [Version]"4.5" }
-                                    "378675|378758" { [Version]"4.5.1" }
-                                    "379893" { [Version]"4.5.2" }
-                                    "393295|393297" { [Version]"4.6" }
-                                    "394254|394271" { [Version]"4.6.1" }
-                                    "394802|394806" { [Version]"4.6.2" }
-                                    "460798" { [Version]"4.7" }
-                                    {$_ -gt 460798} { [Version]"Undocumented 4.7 or higher, please update script" }
+                                    "378389" { "4.5" }
+                                    "378675|378758" { "4.5.1" }
+                                    "379893" { "4.5.2" }
+                                    "393295|393297" { "4.6" }
+                                    "394254|394271" { "4.6.1" }
+                                    "394802|394806" { "4.6.2" }
+                                    "460798" { "4.7" }
+                                    {$_ -gt 460798} { "Undocumented 4.7 or higher, please update script" }
                                 }
                             }
                         }
